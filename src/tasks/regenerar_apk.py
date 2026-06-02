3915958a3f3234d12c57a36e071b01e4a75dbfc5
RakaAmburo <raka.amburo@gmail.com>
2026-06-02 21:32:12 +0200

    fix name centurion

--- src/tasks/regenerar_apk.py
+++ src/tasks/regenerar_apk.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 PROYECTOS = {
     "asistente": {"path": "C:\\repos\\voiceassistant", "nombre": "voiceassistant"},
-    "decurion":  {"path": "C:\\repos\\decurion",       "nombre": "decurion"},
+    "centurion":  {"path": "C:\\repos\\decurion",       "nombre": "decurion"},
 }
 
 TRIGGER_WORDS = ("regenera", "genera el apk", "compila", "regenerar apk", "generar apk")
